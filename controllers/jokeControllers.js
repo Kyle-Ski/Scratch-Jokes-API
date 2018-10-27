38e5c4c4feaa1be198cf699fd543a1d3112d1ca2
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-27 17:06:18 -0600

    added get one route with error handling

--- controllers/jokeControllers.js
+++ controllers/jokeControllers.js
@@ -1,12 +1,13 @@
-const jokes = require('../models/jokeModels')
-const uuid = require('uuid/v4')
+let jokes = require('../models/jokeModels')
 
 const getAll = (req, res, next) => {
     res.json({jokes: jokes})
 }
 
 const getOne = (req, res, next) => {
-
+    let id = req.params.id
+    let theJoke = jokes.filter(joke => joke.id == id)[0]
+    return (!Number(id) || id > jokes.length) ? res.json({error: {status: 400, message: 'Please enter a valid id number'}}) : res.json({joke: theJoke})
 }
 
 const postJoke = (req, res, next) => {
