cc0c2ce51e4d793fda0f7e4a8830689b39ba42e7
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-27 16:55:45 -0600

    competed get all route and started skeleton for the rest

--- controllers/jokeControllers.js
+++ controllers/jokeControllers.js
@@ -1,2 +1,30 @@
 const jokes = require('../models/jokeModels')
 const uuid = require('uuid/v4')
+
+const getAll = (req, res, next) => {
+    res.json({jokes: jokes})
+}
+
+const getOne = (req, res, next) => {
+
+}
+
+const postJoke = (req, res, next) => {
+
+}
+
+const putJoke = (req, res, next) => {
+
+}
+
+const deleteJoke = (req, res, next) => {
+
+}
+
+module.exports = {
+    getAll,
+    getOne,
+    postJoke,
+    putJoke,
+    deleteJoke
+}
--- routes/jokeRoutes.js
+++ routes/jokeRoutes.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../controllers/jokeControllers')
 
+router.get('/', controller.getAll)
+router.get('/:id', controller.getOne)
+router.post('/', controller.postJoke)
+router.put('/:id', controller.putJoke)
+router.get('/:id', controller.deleteJoke)
 
 module.exports = router
